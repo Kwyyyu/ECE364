913a010335502ded786d9ff625ce548348895590
kwyyyu <kwyyyu@gmail.com>
2019-08-23 23:10:49 -0400

    Finish all functions

--- Prelab01/simpleTasks.py
+++ Prelab01/simpleTasks.py
@@ -6,17 +6,19 @@
 #######################################################
 
 import difflib
+import math
+import os
 
 def find(pattern):
     f = open("sequence.txt", "r")
     contents = f.read()
-    sebseq = []
+    subseq = []
     result = []
     for i in range(len(contents) - len(pattern) + 1):
         temp = contents[i:i + len(pattern)]
-        sebseq.append(temp)
-    # print(sebseq)
-    for i in sebseq:
+        subseq.append(temp)
+    # print(subseq)
+    for i in subseq:
         match = 1
         for index in range(len(pattern)):
             if pattern[index] != 'x' and pattern[index] != i[index]:
@@ -29,21 +31,22 @@ def find(pattern):
 
 
 def getStreakProduct(sequence, maxSize, product):
-    sebseq = []
+    subseq = []
     result = []
     for i in range(len(sequence)):
         for j in range(2, maxSize + 1):
             if i + j <= len(sequence):
                 temp = sequence[i:i + j]
-                sebseq.append(temp)
-    # print(sebseq)
-    for i in sebseq:
+                subseq.append(temp)
+    # print(subseq)
+    for i in subseq:
         temp_product = 1
         for j in range(len(i)):
             temp_product = temp_product*int(i[j])
         if temp_product == product:
             result.append(i)
     # print(result)
+    return result
 
 
 def writePyramids(filePath, baseSize, count, char):
@@ -68,15 +71,101 @@ def writePyramids(filePath, baseSize, count, char):
     f = open(filePath,"w")
     f.write(''.join(result))
     f.close()
+    return result
 
 
+def getStreaks(sequence, letters):
+    result = []
+    subseq = []
+    letter = []
+    temp = []
+    for i in range(0, len(letters)):
+        letter.append(letters[i])
+    i = 0
+    while i < len(sequence):
+        j = i+1
+        while j < len(sequence) and sequence[j] == sequence[i]:
+            j += 1
+        temp = sequence[i:j]
+        subseq.append(temp)
+        i = j
+    # print(subseq)
+    for i in range(len(subseq)):
+        for j in letter:
+            if j in subseq[i]:
+                result.append(subseq[i])
+    # print(result)
+    return result
 
+
+def findNames(nameList, part, name):
+    names = []
+    f_name = []
+    l_name = []
+    result = []
+    for i in range(0, len(nameList)):
+        names.append(nameList[i])
+        split_name = nameList[i].split()
+        f_name.append(split_name[0])
+        l_name.append(split_name[1])
+    if part == "L":
+        for i in range(0, len(l_name)):
+            if name.lower() == l_name[i].lower():
+                result.append(names[i])
+    elif part == "F":
+        for i in range(0, len(f_name)):
+            if name.lower() == f_name[i].lower():
+                result.append(names[i])
+    elif part == "FL":
+        for i in range(0, len(f_name)):
+            if name.lower() == f_name[i].lower() or name.lower() == l_name[i].lower():
+                result.append(names[i])
+    return result
+
+
+def convertToBoolean(num, size):
+    result = []
+    if isinstance(num, int):
+        list_num = []
+        i = 0
+        while num != 0 or i < size:
+            if num != 0:
+                bit = num % 2
+                list_num.append(bit)
+                num = int(num / 2)
+            else:
+                list_num.append(0)
+            i += 1
+        list_num.reverse()
+        for i in range(0, len(list_num)):
+            if list_num[i] == 1:
+                result.append(True)
+            else:
+                result.append(False)
+    return result
+
+
+def convertToInteger(boolList):
+    if isinstance(boolList, list) and boolList:
+        for i in boolList:
+            if type(i) != bool:
+                return None
+        result = 0
+        list_num = []
+        for i in range(0, len(boolList)):
+            if boolList[i]:
+                list_num.append(1)
+            else:
+                list_num.append(0)
+        list_num.reverse()
+        for i in range(0, len(list_num)):
+            result = result + list_num[i]*pow(2, i)
+        return result
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    writePyramids("pyramidtest.txt", 15, 5, '*')
-
-    a = open("pyramidtest.txt",'r').readlines()
-    b = open("pyramid15.txt",'r').readlines()
-    diff = difflib.ndiff(a, b)
-    print('\n'.join(diff))
+    names = ["George Smith", "Mark Johnson", "Cordell Theodore", "Maria Satterfield",
+             "Johnson Cadence"]
+    print(findNames(names, "FL", "johnson"))
